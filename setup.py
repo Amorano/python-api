64eedaeaea8124fd8f9a027d743041fc456b82ff
Alexander G. Morano <amorano@gmail.com>
2025-02-15 01:40:08 -0500

    cleanup setup

--- setup.py
+++ setup.py
@@ -9,43 +9,27 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import sys
 from setuptools import setup, find_packages
+from pathlib import Path
 
-
-f = open('README.md')
-readme = f.read().strip()
-
-f = open('LICENSE')
-license = f.read().strip()
-
-script_args = sys.argv[1:]
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='shotgun_api3',
     version='3.8.0',
     description='Flow Production Tracking Python API',
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Autodesk',
-    author_email='https://www.autodesk.com/support/contact-support',
+    author_email='https://developer.shotgridsoftware.com',
     url='https://github.com/shotgunsoftware/python-api',
-    license=license,
+    license="Proprietary",
     packages=find_packages(exclude=('tests',)),
-    script_args=script_args,
     include_package_data=True,
     package_data={'': ['cacerts.txt', 'cacert.pem']},
     zip_safe=False,
     python_requires=">=3.7.0",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Operating System :: OS Independent",
-    ],
     install_requires=[
           'httplib2>=0.19.1',
           'certifi>=2022.12.7',
